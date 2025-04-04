b10d2375ed2c9b0616ecf6b912c3deab5a1bdb76
mattvisa <130477370+mattvisa@users.noreply.github.com>
2025-04-04 15:05:12 -0400

    Update brave-bullshitinator-linux-install.sh

--- brave-bullshitinator-linux-install.sh
+++ brave-bullshitinator-linux-install.sh
@@ -1,11 +1,11 @@
 # Start
-echo "Starting Brave debullshitinator installer"
-echo "Brave debullshitinator is made by Mules Gaming. Find it here: https://github.com/MulesGaming/brave-debullshitinator"
+echo "Starting Brave Debloat installer"
+echo "Brave debloat is forked from Mules Gaming. Find it here: https://github.com/MulesGaming/brave-debullshitinator"
 # Make dir
 sudo mkdir /etc/brave/policies/managed -p
 # Download file
-wget https://github.com/MulesGaming/brave-debullshitinator/releases/download/1.0.4/brave_debullshitinator-policies.json
-sudo cp brave_debullshitinator-policies.json /etc/brave/policies/managed
-rm brave_debullshitinator-policies.json
+wget https://github.com/mattvisa/brave-debloat/releases/download/1.0.4/brave_debloat-policies.json
+sudo cp brave_debloat-policies.json /etc/brave/policies/managed
+rm brave_debloat-policies.json
 # End
-echo "Brave debullshitinator installed"
+echo "Brave Debloat installed"
